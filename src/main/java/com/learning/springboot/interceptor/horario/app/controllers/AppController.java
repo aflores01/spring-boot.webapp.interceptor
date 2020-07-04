ae5460a1bd6cf5724877b69dcaeec16231b34799
karunsane <karunsane@gmail.com>
2020-07-04 13:15:54 -0500

    WebApp Interceptor con excludePath y redirección

--- src/main/java/com/learning/springboot/interceptor/horario/app/controllers/AppController.java
+++ src/main/java/com/learning/springboot/interceptor/horario/app/controllers/AppController.java
@@ -1,16 +1,39 @@
 package com.learning.springboot.interceptor.horario.app.controllers;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class AppController {
+	
+	@Value("${config.horario.apertura}")
+	private Integer apertura;
+
+	@Value("${config.horario.cierre}")
+	private Integer cierre;
 
 	@GetMapping({"/","/index"})
 	private String index(Model model) {
 		model.addAttribute("titulo","Web APP Hour Interceptor");
 		model.addAttribute("subtitulo","WebApp con control de acceso por horario");
+		
 		return "index";
+	}
+	
+	@GetMapping("/cerrado")
+	private String cerrado(Model model) {
+		
+		StringBuilder mensaje = new StringBuilder("Cerrado por favor visitenos desde las: ");
+		mensaje.append(apertura);
+		mensaje.append("hrs. ");
+		mensaje.append("Y hasta las: ");
+		mensaje.append(cierre);
+		mensaje.append("hrs.");
+		mensaje.append("Gracias por su visita.");
+		model.addAttribute("titulo","Fuera de hroario de atención");
+		model.addAttribute("mensaje", mensaje.toString());
+		return "cerrado";
 	}
 }
---
+++ src/main/java/com/learning/springboot/interceptor/horario/app/interceptors/HourInterceptor.java
@@ -0,0 +1,59 @@
+package com.learning.springboot.interceptor.horario.app.interceptors;
+
+import java.util.Calendar;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+@Component("horario")
+public class HourInterceptor implements HandlerInterceptor {
+
+	@Value("${config.horario.apertura}")
+	private Integer apertura;
+
+	@Value("${config.horario.cierre}")
+	private Integer cierre;
+
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+			throws Exception {
+
+		Calendar calendar = Calendar.getInstance();
+		int hora = calendar.get(Calendar.HOUR_OF_DAY);
+
+		if (hora >= apertura && hora < cierre) {
+
+			// Crea strings modificables sin crear más de una instancia
+			StringBuilder mensaje = new StringBuilder("Bienvenido al horario de atención a clientes");
+			mensaje.append(", Atendemos desde las ");
+			mensaje.append(apertura);
+			mensaje.append("hrs. ");
+			mensaje.append("Hasta las ");
+			mensaje.append(cierre);
+			mensaje.append("hrs. ");
+			mensaje.append("Gracias por su visita");
+			request.setAttribute("welcomeMsg", mensaje.toString());
+			return true;
+		}
+
+		response.sendRedirect(request.getContextPath().concat("/cerrado"));
+
+		return false;
+	}
+
+	@Override
+	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
+			ModelAndView modelAndView) throws Exception {
+
+		String mensaje = (String) request.getAttribute("mensaje");
+		if (modelAndView != null) {
+			modelAndView.addObject("horario", mensaje);
+		}
+	}
+
+}
